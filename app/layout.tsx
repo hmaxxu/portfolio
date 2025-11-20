70c3b3ddd6c6ca8d28caa8c6f856e0d814a2e04b
atentwo <atentwo@proton.me>
2025-11-19 23:07:13 -0500

    fix: add ReactNode type to children prop in RootLayout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Cantarell } from "next/font/google";
+import { ReactNode } from "react";
 
 const cantarell = Cantarell({
   weight: ['400', '700'],
@@ -14,7 +15,7 @@ export const metadata: Metadata = {
   description: "",
 };
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     // Apply the class name here
     <html lang="en" className={cantarell.className}> 
